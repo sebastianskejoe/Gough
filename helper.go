3f0e2089da9ee430f3c36b567da919d3554016c3
Sebastian Skejø <sebastianskejoe@gmail.com>
2011-10-08 09:51:40 +0200

    Added type Frame, ColorIsGood() and getImage()

--- helper.go
+++ helper.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"exp/gui"
 	"image/draw"
+	"os"
+	"image/png"
 )
 
 type Window struct {
@@ -14,6 +16,14 @@ type Window struct {
 	Window gui.Window
 	Ppc int // pixel per centimeter
 	Dist int // distance from camera to track point
+	Cfra int // current frame
+	Frames []Frame
+}
+
+type Frame struct {
+	Path string
+	Id int
+	Centre Circle
 }
 
 func filter(img *image.Image, filtered chan<- *image.Point) {
@@ -22,8 +32,7 @@ func filter(img *image.Image, filtered chan<- *image.Point) {
 	sent := 0
 	for x := 0 ; x < bx ; x++ {
 		for y := 0 ; y < by ; y++ {
-			r,_,_,_ := (*img).At(x,y).RGBA()
-			if r > 200 {
+			if ColorIsGood((*img).At(x,y)) {
 				filtered <- &image.Point{x,y}
 				sent++
 			}
@@ -33,6 +42,14 @@ func filter(img *image.Image, filtered chan<- *image.Point) {
 	close(filtered)
 }
 
+func ColorIsGood(c image.Color) bool {
+	r,_,_,_ := c.RGBA()
+	if r > 200 {
+		return true
+	}
+	return false
+}
+
 func findBounds(img *image.Image) image.Rectangle {
 	bx := (*img).Bounds().Max.X
 	by := (*img).Bounds().Max.Y
@@ -40,8 +57,7 @@ func findBounds(img *image.Image) image.Rectangle {
 
 	for x := 0 ; x < bx ; x++ {
 		for y := 0 ; y < by ; y++ {
-			r,_,_,_ := (*img).At(x,y).RGBA()
-			if r > 200 {
+			if ColorIsGood((*img).At(x,y)) {
 				if x > maxx { maxx = x }
 				if x < minx { minx = x }
 				if y > maxy { maxy = y }
@@ -51,4 +67,19 @@ func findBounds(img *image.Image) image.Rectangle {
 	}
 
 	return image.Rect(minx,miny,maxx,maxy)
+}
+
+func getImage(path string) image.Image {
+	// Get image
+	file,err := os.Open(path)
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+	}
+	defer file.Close()
+
+	img,err := png.Decode(file)
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+	}
+	return img
 }
